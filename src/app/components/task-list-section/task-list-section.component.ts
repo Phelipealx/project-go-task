d9f06a3936075ccc29da3ca5637037d489949bbb
Phelipe Magalhães <phelipemagalhaes13@gmail.com>
2026-07-16 19:29:13 -0300

    add drag and drop and async pipe to subscribe to task list service

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,19 +1,39 @@
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, inject, OnInit } from '@angular/core';
+import { ITask } from '../../interfaces/task.interface';
 import { TaskService } from '../../services/task.service';
 import { TaskCardComponent } from '../task-card/task-card.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag, AsyncPipe],
   templateUrl: './task-list-section.component.html',
-  styleUrl: './task-list-section.component.css',
+  styleUrls: ['./task-list-section.component.css'],
 })
-export class TaskListSectionComponent implements OnInit {
-  private readonly _taskService = inject(TaskService);
+export class TaskListSectionComponent {
+  protected readonly _taskService = inject(TaskService);
 
-  ngOnInit(): void {
-    this._taskService.todoTasks.subscribe((tasks) => {
-      console.log('TODO Tasks:', tasks);
-    });
+  protected drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
